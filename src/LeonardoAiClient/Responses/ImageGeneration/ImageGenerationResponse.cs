a0ac98b1a89210b9c6c07de8b79de79d260329ea
imaun <imun22@gmail.com>
2025-01-11 23:53:45 +0100

    add ImageGeneration responde model

--- src/LeonardoAiClient/Responses/ImageGeneration/ImageGenerationResponse.cs
+++ src/LeonardoAiClient/Responses/ImageGeneration/ImageGenerationResponse.cs
@@ -1,6 +1,18 @@
 namespace LeonardoAiClient.Responses;
 
+using System.Text.Json.Serialization;
+
 public class ImageGenerationResponse
 {
-    
+    [JsonPropertyName("sdGenerationJob")]
+    public SdGenerationJob? SdGenerationJob { get; set; }
+}
+
+public class SdGenerationJob
+{
+    [JsonPropertyName("generationId")]
+    public string GenerationId { get; set; }
+
+    [JsonPropertyName("apiCreditCost")]
+    public int? ApiCreditCost { get; set; }
 }
