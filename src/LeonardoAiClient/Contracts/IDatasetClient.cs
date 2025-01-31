4e6ea3952a5a9aa069e4a5bba2f254d78de54110
imaun <imun22@gmail.com>
2025-01-31 23:43:30 +0100

    add UploadDatasetImage to contract

--- src/LeonardoAiClient/Contracts/IDatasetClient.cs
+++ src/LeonardoAiClient/Contracts/IDatasetClient.cs
@@ -31,4 +31,16 @@ public interface IDatasetClient
     /// <returns>A task that represents the asynchronous operation.</returns>
     Task DeleteDatasetByIdAsync(string datasetId, CancellationToken cancellationToken = default);
 
+    
+    /// <summary>
+    /// Uploads an image to a dataset and retrieves presigned details for uploading to S3.
+    /// </summary>
+    /// <param name="datasetId">The ID of the dataset to which the image will be uploaded.</param>
+    /// <param name="request">The request containing the image file extension.</param>
+    /// <param name="cancellationToken">The cancellation token to cancel the operation.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the response with presigned upload details.</returns>
+    Task<UploadDatasetImageResponse?> UploadDatasetImageAsync(
+        string datasetId,
+        UploadDatasetImageRequest request,
+        CancellationToken cancellationToken = default);
 }
