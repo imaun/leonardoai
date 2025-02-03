d3903892729bd03d295b18cb46ca48bc55c526f3
imaun <imun22@gmail.com>
2025-02-03 20:37:21 +0100

    add LcmGeneration to LeonardoClient

--- src/LeonardoAiClient/LeonardoAiClient.cs
+++ src/LeonardoAiClient/LeonardoAiClient.cs
@@ -14,6 +14,7 @@ public class LeonardoAiClient : ILeonardoAiClient
         Prompt = new PromptClient(httpClientFactory, apiKey);
         Dataset = new DatasetClient(httpClientFactory, apiKey);
         Model = new ModelClient(httpClientFactory, apiKey);
+        LcmGeneration = new LcmGenerationClient(httpClientFactory, apiKey);
     }
     
     public IImageGenerationsClient ImageGeneration { get; }
@@ -25,4 +26,6 @@ public class LeonardoAiClient : ILeonardoAiClient
     public IDatasetClient Dataset { get; }
     
     public IModelClient Model { get; }
+    
+    public ILcmGenerationClient LcmGeneration { get; }
 }
